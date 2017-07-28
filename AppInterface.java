9e4c9ac67a2e6cf6931f0609663a9ddf115b17de
carriemn00 <carrieand99@gmail.com>
2017-07-28 14:35:10 -0700

    Update AppInterface.java

--- AppInterface.java
+++ AppInterface.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 
 public class AppInterface implements ActionListener{
 	JFrame frame;
+	JFrame frame2;
     JLabel label;
     int mixerChoice = 0;
     TestSoundRecordingUtil output = new TestSoundRecordingUtil();
@@ -28,10 +29,13 @@ public class AppInterface implements ActionListener{
         
     	frame = new JFrame("Beginning Guitarists App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2 = new JFrame("Fret");
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JPanel helpPanel = new JPanel();
         JPanel panel = new JPanel();
         JPanel panel2 = new JPanel();
+        JPanel imagePanel = new JPanel();
  
         JButton startButton = new JButton("Begin audio capture");
         startButton.addActionListener(new StartListener());
@@ -41,19 +45,27 @@ public class AppInterface implements ActionListener{
         
         JButton helpButton = new JButton("Help");
         helpButton.addActionListener(new HelpListener());
+        
+        JButton promptButton = new JButton("Note Prompt");
+        promptButton.addActionListener(new PromptListener());
  
         label = new JLabel("Welcome", SwingConstants.CENTER);
- 
+    	ImageIcon icon = new ImageIcon("fret.jpg");
+    	JLabel imagelabel = new JLabel(icon);
+    	
         // add buttons and labels to panels 
         panel.add(startButton);
         panel.add(resetButton);
         helpPanel.add(helpButton);
+        helpPanel.add(promptButton);
         panel2.add(label);
+        imagePanel.add(imagelabel);
         
         // add panels to frame
         frame.getContentPane().add(BorderLayout.NORTH, helpPanel);
         frame.getContentPane().add(BorderLayout.SOUTH, panel);
         frame.getContentPane().add(BorderLayout.CENTER, panel2);
+        frame2.getContentPane().add(BorderLayout.CENTER, imagePanel);
         Container center = new Container();
         
         // create mixer buttons
@@ -74,6 +86,8 @@ public class AppInterface implements ActionListener{
         
         frame.setSize(900, 500);
         frame.setVisible(true);
+        frame2.setSize(900, 500);
+        frame2.setVisible(true);
     }
  
     /**
@@ -81,10 +95,13 @@ public class AppInterface implements ActionListener{
      */
     public class StartListener implements ActionListener{
         public void actionPerformed(ActionEvent event){
-        	//call record audio method
-                label.setText("Beginning audio capture...");
-                String labelOutput = output.getTestSoundRecordingUtil();
-                label.setText(labelOutput);
+        	//call prompter
+        	label.setText("");
+        	Prompt output = new Prompt();
+        	String labelOutput = output.Prompter();
+        	label.setHorizontalAlignment(SwingConstants.CENTER);
+        	label.setVerticalAlignment(SwingConstants.CENTER);
+        	label.setText(labelOutput);
         }
     }
     ///////// WORK ON RESET METHOD /////////
@@ -107,7 +124,19 @@ public class AppInterface implements ActionListener{
     
         }
     }
-
+    
+    class PromptListener implements ActionListener{
+    	public void actionPerformed(ActionEvent event) {
+    		// call prompter
+    		label.setText("");
+        	Prompt output = new Prompt();
+        	String labelOutput = output.Prompter();
+        	label.setHorizontalAlignment(SwingConstants.CENTER);
+        	label.setVerticalAlignment(SwingConstants.CENTER);
+        	label.setText(labelOutput);
+    	}
+    }
+    
     /**
      * Get mixer choice from user
      */
