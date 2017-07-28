2fa5d791fbd7fcdd1e4c826273c0f37d01837188
carriemn00 <carrieand99@gmail.com>
2017-07-28 11:52:41 -0700

    Update AppInterface.java

--- AppInterface.java
+++ AppInterface.java
@@ -1,28 +1,29 @@
- /* Guitar App Interface
+/* Guitar App Interface
  *  Team 9
  */
+
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Mixer;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//import java.util.Timer;
-//import java.util.TimerTask;
 
- 
 public class AppInterface implements ActionListener{
 	JFrame frame;
     JLabel label;
     int mixerChoice = 0;
     TestSoundRecordingUtil output = new TestSoundRecordingUtil();
     
- 
+
     public static void main(String[] args) {
         AppInterface gui = new AppInterface();
         gui.go();
     }
  
+    /**
+     * Declare and initialize GUI components
+     */
     public void go(){
         
     	frame = new JFrame("Beginning Guitarists App");
@@ -33,11 +34,9 @@ public class AppInterface implements ActionListener{
         JPanel panel2 = new JPanel();
  
         JButton startButton = new JButton("Begin audio capture");
-        startButton.setSize(100,200);
         startButton.addActionListener(new StartListener());
  
         JButton resetButton = new JButton("Reset");
-        resetButton.setSize(700,200);
         resetButton.addActionListener(new ResetListener());
         
         JButton helpButton = new JButton("Help");
@@ -45,18 +44,21 @@ public class AppInterface implements ActionListener{
  
         label = new JLabel("Welcome", SwingConstants.CENTER);
  
+        // add buttons and labels to panels 
         panel.add(startButton);
         panel.add(resetButton);
         helpPanel.add(helpButton);
         panel2.add(label);
+        
+        // add panels to frame
         frame.getContentPane().add(BorderLayout.NORTH, helpPanel);
         frame.getContentPane().add(BorderLayout.SOUTH, panel);
         frame.getContentPane().add(BorderLayout.CENTER, panel2);
         Container center = new Container();
         
+        // create mixer buttons
         Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
 		center.setLayout(new GridLayout(mixerInfo.length+1, 1));
-		//center.add(label);
 		JButton[] buttons = new JButton[mixerInfo.length];
 		for (int i = 0; i < mixerInfo.length; i++) {
 			buttons[i] = new JButton(i + ". "+mixerInfo[i]);
@@ -66,7 +68,7 @@ public class AppInterface implements ActionListener{
 			System.out.println(i + ". " + mixerInfo[i]);
 		
 		}
-		
+		// add mixer buttons to frame
 		frame.getContentPane().add(BorderLayout.WEST, center);
         
         
@@ -74,16 +76,18 @@ public class AppInterface implements ActionListener{
         frame.setVisible(true);
     }
  
+    /**
+     * Action listeners respond to button events
+     */
     public class StartListener implements ActionListener{
         public void actionPerformed(ActionEvent event){
         	//call record audio method
                 label.setText("Beginning audio capture...");
-                //TestSoundRecordingUtil output = new TestSoundRecordingUtil();
                 String labelOutput = output.getTestSoundRecordingUtil();
                 label.setText(labelOutput);
         }
     }
-    
+    ///////// WORK ON RESET METHOD /////////
     class ResetListener implements ActionListener{
     	public void actionPerformed(ActionEvent event){
     		//call reset method
@@ -93,7 +97,7 @@ public class AppInterface implements ActionListener{
  
     class HelpListener implements ActionListener{
         public void actionPerformed(ActionEvent event){
-        	// call Help method
+        	// call Help 
         	label.setText("");
         	Help output = new Help();
         	String labelOutput = output.getHelp();
@@ -102,19 +106,15 @@ public class AppInterface implements ActionListener{
         	label.setText(labelOutput);
     
         }
-        
-    class MixerListener implements ActionListener {
-        	public void actionPerformed(ActionEvent event) {
-        		label.setText("Working");
-        	
-    		}
-    	}
     }
 
+    /**
+     * Get mixer choice from user
+     */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonPressed = (JButton)e.getSource();
-		//buttons.setVisible(false); FIX
+		// buttons.setVisible(false); FIX
 		buttonPressed.setVisible(true);
 		mixerChoice = Integer.parseInt(buttonPressed.getText().substring(0,1));
 		output.recorder.setMixerChoice(mixerChoice);
