35f8f9cb4949b0aab516bbae160e8db7e6292f5a
carriemn00 <carrieand99@gmail.com>
2017-08-02 11:15:02 -0700

    Update Help.java

--- Help.java
+++ Help.java
@@ -1,15 +1,26 @@
-/* Help 
- * Carrie Nguyen
+* Help 
+ * Team 9
  * 7/24/17
  */
 
 public class Help {
 	public String getHelp() {
-		String helpInfo = "<html>Welcome to the Beginning Guitar App<BR>"
-				+ "To record sound, click \"Begin audio capture\" and play a note."
-				+ "<BR>Click \"Stop audio capture\" to stop recording."
-				+ "<BR>The note played will be identified and displayed."
-				+ "<BR>Have fun!<html>";
+		String helpInfo = "<html> Welcome to the Beginning Guitar App <BR>"
+				+ "<BR>"
+				+ "1. Choose a mixer by clicking one of the buttons on the left. <BR>"
+				+ "<BR>"
+				+ "2. Click \"Start \" to receive a note to play. <BR>"
+				+ "<BR>"
+				+ "3. Play the note as prompted onscreen.<BR>"
+				+ "<BR>"
+				+ "4. The note you play will be identified and displayed. <BR>"
+				+ "<BR>"
+				+ "5. Click \"Reset \" to clear the screen. Select a mixer and press start to begin again. <BR>"
+				+ "<BR>"
+				+ "6. To use the tuner, click \"Tune \" to begin. Tune the first string according to the output onscreen "
+				+ "and then click \"Next String\" to tune the next string on the guitar. <BR>"
+				+ "<BR>"
+				+ "7. Have fun! <html>";
 
 		// add more user instructions here
 		return helpInfo;
