4172f713a48042a0089f2d3c91bfd83ac1f6ae20
carriemn00 <carrieand99@gmail.com>
2017-08-09 10:17:19 -0700

    Update Help.java
    Cleaned and finalized

--- Help.java
+++ Help.java
@@ -1,25 +1,30 @@
 /* Help 
- * Team 9
- * 7/24/17
+ * Resonance
+ * NAPW 2017 - Team 9
  */
 
 public class Help {
 	public String getHelp() {
-		String helpInfo = "<html>Welcome to the Beginning Guitar App<BR>"
+		String helpInfo = "<html> 1. Choose a mixer (where your guitar/mic is plugged in) <BR>"
+				+ "by selecting one from the top left drop down menu. <BR>"
 				+ "<BR>"
-				+ "1. Choose a mixer (where your guitar/mic is plugged in) by selecting one from the top left drop down menu. <BR>"
-				+ "<BR>"
-				+ "2. To practice note locating, click \"Start note recognition\" and play the prompted note.<BR>"
+				+ "2. To practice note locating, click \"Start note recognition\" and <BR>"
+				+ "play the prompted note.<BR>"
 				+ "<BR>"
 				+ "3. Click \"Stop\" to end note location practice.<BR>"
 				+ "<BR>"
-				+ "4. To practice fretboard familiarity, click \"Start fretboard practice\" and play the prompted string and fret.<BR>"
+				+ "4. To practice fretboard familiarity, click \"Start fretboard <BR>"
+				+ "practice\" and play the prompted string and fret.<BR>"
 				+ "<BR>"
 				+ "5. Click \"Stop\" to end fretboard familiarity practice.<BR>"
 				+ "<BR>"
-				+ "6. To tune the guitar, click \"Tune\". Start tuning the lowest string. Click \"Next\" to move to the next string.<BR>"
+				+ "6. To tune the guitar, click \"Tune\". Start tuning the lowest <BR>"
+				+ "string. Click \"Next\" to move to the next string.<BR>"
 				+ "<BR>"
-				+ "7. Have fun!<html>";
+				+ "7. Audio input will be displayed visually in the waveform <BR>"
+				+ "display in the upper right corner. <BR>"
+				+ "<BR>"
+				+ "8. Have fun!<html>";
 
 		// add more user instructions here
 		return helpInfo;
